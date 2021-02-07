1c3780e5faa39808861f09a515751f0943ec5b9c
Abderrezak FENOUCHE <abfenouche@gmail.com>
2021-02-07 11:49:39 +0100

    login OK

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { Button, FormGroup, FormControl, FormLabel, Form } from "react-bootstrap";
+//import { Button, FormGroup, FormControl, FormLabel, Form } from "react-bootstrap";
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
 import API from "../../utils/API";
 
 export class Login extends React.Component {
-  state = {
-    email: "",
-    password: ""
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    
+    };
+  }
   send = async () => {
     const { email, password } = this.state;
     if (!email || email.length === 0) {
-      return;
+      return console.log("fuck emaikl");
     }
     if (!password || password.length === 0) {
-      return;
+      return console.log("fuck password");
     }
     try {
       const { data } = await API.login(email, password);
@@ -24,43 +30,39 @@ export class Login extends React.Component {
       console.error(error);
     }
   };
-  handleChange = (event) => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
+  handleChangeE = (event) => {
+    this.setState({email: event.target.value});
   };
+  handleChangeP = (event) => {
+    this.setState({password: event.target.value});
+  };
+  
   render() {
-    const { email, password } = this.state;
     return (
       <div className="Login">
-        <h4 className="text-center mb-5">Authentification</h4>
-        <Form onSubmit={this.send}> 
-          <FormGroup controlId="email" >
-            <label>Nom d'utilisateur</label>
-            <FormControl
-              autoFocus
-              type="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" >
-            <FormLabel>Mot de passe</FormLabel>
-            <FormControl
-              value={password}
-              onChange={this.handleChange}
-              type="password"
-            />
-          </FormGroup>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol md="12">
+              <form onSubmit={this.send}>
+                <p className="h5 text-center mb-4">Authentification</p>
+                <div className="grey-text">
+                  <MDBInput label="Tapez votre email" icon="envelope" group type="email" validate error="wrong" 
+                    value={this.state.email} onChange={this.handleChangeE.bind(this)} success="right" />
+                  <MDBInput label="Tapez votre mot de passe" icon="lock" group type="password" 
+                    value={this.state.password} onChange={this.handleChangeP.bind(this)} validate />
+                </div>
+                <div className="text-center">
+                  <MDBBtn type="submit">Valider</MDBBtn>
+                </div>
+              </form>
+            </MDBCol>
+          </MDBRow>
           <br></br>
-          <Button className="btn-success"  block bssize="large" type="submit">
-            Connexion
-        </Button>
-        </Form>
-
-        <br></br>
         <label className="mt-3">Vous n'avez pas encore de compte ?</label>
         <a className=" m-2" href="/signup">Créez en un </a>
+        </MDBContainer>
+
+        
       </div>
 
     );
